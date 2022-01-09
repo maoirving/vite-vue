52ad58ccc992346dbcb1b5af7f611a42981d7b96
mao <1559304351@qq.com>
2022-01-09 11:19:57 +0800

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-essential', 'standard'],
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    quotes: [1, 'single'], // 引号
+    'space-before-function-paren': 0, // 函数前空格
+    'comma-dangle': ['error', 'only-multiline'], // 拖尾逗号
+    'max-len': ['error', { code: 200 }], // 强制最大行长度
+  },
+}
