67913d662c4f570e7e5c2c6a2824e61747b487b3
mao <1559304351@qq.com>
2022-02-25 00:15:09 +0800

    feat: demo

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,3 +1,14 @@
+<template>
+  <h1>{{ title }}</h1>
+  <img alt="Vue logo" src="../assets/logo.png" />
+  <router-link to="/products">product lister</router-link>
+  <BaseButton variant="primary" @click="setTitle">hello</BaseButton>
+  <div class="container">
+    <div class="left"></div>
+    <div class="right"></div>
+  </div>
+</template>
+
 <script lang="ts">
 import { defineComponent, reactive, ref, toRef } from 'vue'
 import BaseButton from '@/components/base/Button.vue'
@@ -32,9 +43,19 @@ export default defineComponent({
 })
 </script>
 
-<template>
-  <h1>{{ title }}</h1>
-  <img alt="Vue logo" src="../assets/logo.png" />
-  <router-link to="/products">product lister</router-link>
-  <BaseButton variant="primary" @click="setTitle">hello</BaseButton>
-</template>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  width: 600px;
+  height: 500px;
+  background-color: pink;
+  .left {
+    flex: 1 2 300px;
+    background-color: yellow;
+  }
+  .right {
+    flex: 2 1 200px;
+    background-color: skyblue;
+  }
+}
+</style>
